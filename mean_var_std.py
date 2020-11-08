876128b7e0f39f9806dd0b93cc5ff09202968e87
Jason Tolbert <jasonalantolbert@gmail.com>
2020-11-07 20:26:12 -0500

    initial commit

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,24 @@
 import numpy as np
 
-def calculate(list):
 
+def calculate(num_list):
+    if len(num_list) < 9:
+        raise ValueError("List must contain nine numbers.")
 
+    modes = {"mean": "mean",
+             "var": "variance",
+             "std": "standard deviation",
+             "max": "max",
+             "min": "min",
+             "sum": "sum"}
 
+    data = {}
 
-    return calculations
+    array = np.asarray(num_list)
+    array.shape = (3, 3)
+
+    for mode in modes.keys():
+        data[modes[mode]] = \
+            eval(f"[list(array.{mode}(axis=0)), list(array.{mode}(axis=1)), array.flatten().{mode}()]")
+
+    return data
